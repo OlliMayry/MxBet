b0d828d5288d5eb4994a88f44f4ae90fda0403e2
OlliMayry <91195131+OlliMayry@users.noreply.github.com>
2021-11-30 18:55:59 +0200

    Create dbh.inc.php

---
+++ dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$serverName = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dbName = "MxBet";
+
+$conn = mysqli_connect($serverName,$dBUsername,$dBPassword,$dbName)
+
+if(!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
