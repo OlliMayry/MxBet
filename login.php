ab5c006a78f03a0f60b5233ef8870ee9e76728fb
OlliMayry <olli.mayry@gmail.com>
2021-12-02 14:40:30 +0200

    ..

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ include_once 'header.php';
 <h2>Login to MxBet</h2>
 <div class="signup-form-form">
 <form action="login.inc.php" method="post">
-<input type="text" name="uid" placeholder="Username...">
+<input type="text" name="name" placeholder="Username/Email...">
 <input type="password" name="pwd" placeholder="Password...">
 <button type="submit" name="submit">Login</button>
 </form>
