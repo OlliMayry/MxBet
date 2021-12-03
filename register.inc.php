82dfb78216e0a8f95b721f4e03c6748b45d27967
OlliMayry <olli.mayry@gmail.com>
2021-12-03 13:35:11 +0200

    ..

--- register.inc.php
+++ register.inc.php
@@ -10,8 +10,33 @@ $pwdRepeat = $_POST["pwdrepeat"];
 require_once 'dbh.inc.php';
 require_once 'functions.inc.php';
 
+if(emptyInputRegister($name, $email, $username, $pwd, $pwdRepeat) !== false) {
+  header("location: ../register.php?error=emptyinput");
+  exit();
+}
+if(invalidUid($username) !== false) {
+  header("location: ../register.php?error=invalidUid");
+  exit();
+}
+if(invalidEmail($email) !== false) {
+  header("location: ../register.php?error=invalidEmail");
+  exit();
+}
+if(pwdMatch($pwd, $pwdRepeat) !== false) {
+  header("location: ../register.php?error=passwordsdontmatch");
+  exit();
+}
+if(uidExsists($conn, $username, $email) !== false) {
+  header("location: ../register.php?error=usernametaken");
+  exit();
+}
+
+createUser($conn, $name, $username, $email, $pwd);
 
 }
+
 else{
-  header("location: ../register.php")
+  header("location: ../register.php");
+  exit();
+}
 }
